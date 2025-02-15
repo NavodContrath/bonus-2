4501447a2221674cc2b9c1792c64e4b7ded0fc00
NavodContrath <navod.contrath@gmail.com>
2025-02-15 20:28:42 +0100

    add function

--- trovaMassimo(arr)/main.js
+++ trovaMassimo(arr)/main.js
@@ -7,5 +7,23 @@ Scrivi una funzione trovaMassimo(arr) che accetti un array di numeri e restituis
 - function findMaxValue (for loop)+(IF)
 */
 const numbersList = [1, 2, 3, 44, 5, 66, 7, 8, 9, 10]
+//max value use to compare with the elements in the array
 let maxNumber = numbersList[0]
+//function
+function findMaxValue() {
+    //for loop to check each element
+    for (let i = 0; i < numbersList.length; i++) {
+        const thisNumber = numbersList[i]
+        //IF element of the list is bigger than the value of the var, var and element will be equal
+        if (thisNumber > maxNumber) {
+            maxNumber = thisNumber;
+        }
+    }
+    return maxNumber
+}
+findMaxValue()
+const maxNumberFound = maxNumber
+console.log(maxNumberFound);
+
+
 
