513bc43322155dd0d88639c997a13e5729b6e5cd
NavodContrath <navod.contrath@gmail.com>
2025-02-15 20:09:44 +0100

    add maxNumber variable

--- trovaMassimo(arr)/main.js
+++ trovaMassimo(arr)/main.js
@@ -7,3 +7,5 @@ Scrivi una funzione trovaMassimo(arr) che accetti un array di numeri e restituis
 - function findMaxValue (for loop)+(IF)
 */
 const numbersList = [1, 2, 3, 44, 5, 66, 7, 8, 9, 10]
+let maxNumber = numbersList[0]
+
