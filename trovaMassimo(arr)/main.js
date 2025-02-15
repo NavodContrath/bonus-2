e2dd2ae1fd8110f78ee80336edbbc9ef4116832d
NavodContrath <navod.contrath@gmail.com>
2025-02-15 20:07:53 +0100

    first commit

---
+++ trovaMassimo(arr)/main.js
@@ -0,0 +1,9 @@
+/* 
+Scrivi una funzione trovaMassimo(arr) che accetti un array di numeri e restituisca il valore massimo presente. */
+
+/*SUMMARY
+- array of number
+- const with array value
+- function findMaxValue (for loop)+(IF)
+*/
+
