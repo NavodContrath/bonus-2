ec88552641d762e7331ed983c659b8c0a652d429
NavodContrath <navod.contrath@gmail.com>
2025-02-16 13:22:54 +0100

    add randomGenerator task with array

---
+++ generaCasuali(n, min, max)/main.js
@@ -0,0 +1,9 @@
+/* 
+Scrivi una funzione generaCasuali(n, min, max) che restituisca un array di n numeri casuali compresi tra min e max */
+
+/*SUMMARY
+- empty array
+- function randomGenerator(for loop x times)+(element push)
+*/
+//array
+const numbersArray = []
